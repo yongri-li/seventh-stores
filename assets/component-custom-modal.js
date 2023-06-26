3d06a2fde606d3f244fc2fb60288ce65fe70a4a1
yongri-li <yongri.liii+1@gmail.com>
2023-06-26 16:33:36 +0100

    Creating a new Modal so we can phase out Alpine JS

---
+++ assets/component-custom-modal.js
@@ -0,0 +1,53 @@
+class CustomModal {
+
+  constructor() {
+
+    super();
+
+    document.addEventListener('keyup', (e) => {
+
+      if (e.key === "Escape") {
+        if (this.classList.contains('active')) {
+          this.close();
+        }
+      }
+    });
+  }
+
+  open() {
+
+    this.classList.add('active');
+    document.querySelector('body')?.classList.add('overflow-hidden');
+  }
+
+  close() {
+
+    this.classList.remove('active');
+    document.querySelector('body')?.classList.remove('overflow-hidden');
+  }
+}
+
+customElements.define('custom-modal', CustomModal);
+
+class ModalTrigger {
+
+  constructor() {
+
+    super();
+
+    this.target = document.querySelector(`[data-modal="${this.dataset.target}"]`) || false;
+
+    this.querySelector('button').addEventListener('click', (e) => {
+
+      if (!this.target) return;
+
+      if (this.target.classList.contains('active')) {
+        this.target.close();
+      } else {
+        this.target.open();
+      }
+    });
+  }
+}
+
+customElements.define('modal-trigger', ModalTrigger);
